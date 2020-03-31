94ca93add8c7f4325f3ac7ea3f87510dbbe57b62
gremliiin <rekufo@gmail.com>
2020-03-31 19:12:56 +0600

    Examples

---
+++ selection_sort/main.js
@@ -0,0 +1,34 @@
+let arr = [];
+
+let arrLength = +prompt("Choose the length of the array", 0);
+
+for (let i = 0; i < arrLength; i++){
+    let arrPush = +prompt("Enter the number", 0);
+    arr.push(arrPush);
+}
+
+let findSmallest = function (arr){
+    let smallest = arr[0];
+    let smallestIndex = 0;
+        for (let i = 1; i < arr.length; i++ ){
+            if (arr[i] < smallest) {
+                smallest = arr[i];
+                smallestIndex = i;
+            }
+        }
+    return smallestIndex;
+};
+console.log(findSmallest(arr));
+
+let selectionSort = function(arr){
+    let newArr = [];
+    let arrLength = arr.length;
+    for (let i = 0; i < arrLength; i++){
+        let smallest = findSmallest(arr);
+        newArr.push(arr.splice(smallest, 1));
+    }
+
+    return newArr;
+};
+
+$("#output").text("Your array sorted: " + selectionSort(arr).join(" "));
